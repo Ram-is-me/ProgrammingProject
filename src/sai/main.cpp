be7833e6ba961a5ad85ba1aef2743abd67a13752
sai venkatesh <suryasai.venkatesh@gmail.com>
2018-11-29 22:46:45 +0530

    got correct evalues

--- src/sai/main.cpp
+++ src/sai/main.cpp
@@ -26,19 +26,16 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-    vector<vector<double>> temp=power_itration(mat,5);
+    vector<double> value;
+    vector<vector<double>> temp=power_itration(mat,5,value);
     for(i1=0;i1<5;i1++)
     {
         for(i2=0;i2<5;i2++)
         {
             cout<<temp[i1][i2]<<" ";
         }
+        cout<<"     value   "<<value[i1];
         cout<<endl;
-    }
-    vector<double> temp1= get_evalues(temp);
-    for(i1=0;i1<temp1.size();i1++)
-    {
-        cout<<endl<<temp1[i1]<<" ";
     }
     cout<<endl;
 }
